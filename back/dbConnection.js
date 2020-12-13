fb9e69fc2f0f0a7287e9e5a00955ac6b48df59fa
unknown <mbenhamouda099@gmail.com>
2020-12-13 20:44:06 +0100

    implement Pusher

--- back/dbConnection.js
+++ back/dbConnection.js
@@ -1,5 +1,13 @@
 var mongoose = require('mongoose');
+const Pusher = require('pusher');
 
+const pusher = new Pusher({
+  appId: "1122523",
+  key: "6e0436202af424d9905b",
+  secret: "0e47e21e112fd2f915e0",
+  cluster: "eu",
+  useTLS: true
+});
 // Db config 
 
 const Connection_url="mongodb+srv://admin:sispoof@cluster0.3rdbn.mongodb.net/ChatApp?retryWrites=true&w=majority";
@@ -20,6 +28,17 @@ db.once('open',()=>{
 
   changeStream.on("change",(change)=>{
     console.log('something is changed: ', change);
+
+    if(change.operationType==="insert")
+    {
+      const messsageDetails = change.fullDocument;
+      pusher.trigger('message','inserted',
+      {
+        name:messsageDetails.name,
+        message:messsageDetails.message,
+     
+      });
+    }else{console.log('error on puhser trigger ! ');}
   });
 
 })
